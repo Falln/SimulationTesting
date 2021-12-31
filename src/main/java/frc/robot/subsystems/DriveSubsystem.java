1461b4d68c6ee2e6886b4c37e6c9be870a440169
Falln <isaaccole8806@gmail.com>
2021-12-31 13:57:09 -0600

    Finished DriveSim and added an extra comment
    DriveSim now uses the 2022 REV and NavX libraries and simulates a KOP chassis using these sensors/motor controller

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -106,6 +106,7 @@ public class DriveSubsystem extends SubsystemBase {
 
     driveSim.update(0.02);
 
+    //update navX and Spark data (as much as needed)
     setSimDoubleFromDeviceData("navX-Sensor[0]", "Yaw", driveSim.getHeading().getDegrees());
     setSimDoubleFromDeviceData("SPARK MAX [4]", "Position", driveSim.getLeftPositionMeters());
     setSimDoubleFromDeviceData("SPARK MAX [5]", "Position", driveSim.getRightPositionMeters());
